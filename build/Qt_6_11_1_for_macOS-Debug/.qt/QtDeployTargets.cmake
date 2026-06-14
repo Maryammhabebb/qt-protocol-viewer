b7d0a6e3a0456064b901d7d6316ee8de0080c6aa
Maryammhabebb <maryamhabeb6803@gmail.com>
2026-06-14 03:20:31 +0300

    Add .gitignore, remove build artifacts from tracking

--- build/Qt_6_11_1_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_qt-protocol-viewer_FILE /Users/maryamhabeb/qt-protocol-viewer/build/Qt_6_11_1_for_macOS-Debug/qt-protocol-viewer.app/Contents/MacOS/qt-protocol-viewer)
-set(__QT_DEPLOY_TARGET_qt-protocol-viewer_TYPE EXECUTABLE)
--- build/Qt_6_11_1_for_macOS-Debug/CMakeFiles/qt-protocol-viewer_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/qt-protocol-viewer_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
